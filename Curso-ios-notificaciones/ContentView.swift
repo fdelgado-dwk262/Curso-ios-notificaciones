00c2a97b5951ca3c9acac79aff5a6d422b9783dd
Fernando <dwk262@gmail.com>
2026-02-25 13:47:30 +0100

    Initial Commit

---
+++ Curso-ios-notificaciones/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Curso-ios-notificaciones
+//
+//  Created by Equipo 9 on 25/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Curso-ios-notificaciones/Curso_ios_notificacionesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Curso_ios_notificacionesApp.swift
+//  Curso-ios-notificaciones
+//
+//  Created by Equipo 9 on 25/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Curso_ios_notificacionesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
