5ab543fe811f275df4a934ae0626bb55d45d71f8
Fernando <dwk262@gmail.com>
2026-02-26 12:38:13 +0100

    Notificaciones parte 2 final

--- Curso-ios-notificaciones/NotificationManager.swift
+++ Curso-ios-notificaciones/NotificationManager.swift
@@ -10,22 +10,111 @@ import UserNotifications
 
 @Observable
 class NotificationManager: NSObject, UNUserNotificationCenterDelegate {
-    
+
     var estadoDeLaAutorizacion: UNAuthorizationStatus = .notDetermined
-    
+
+    var idRecordatorio: String?
+
     override init() {
         super.init()
         // un singelton .-
         UNUserNotificationCenter.current().delegate = self
-        
+
         Task {
             await comprobarEstadoDePermisos()
         }
     }
-    
+
+    @MainActor
     func comprobarEstadoDePermisos() async {
-        let settings = await UNUserNotificationCenter.current().notificationSettings()
+        let settings = await UNUserNotificationCenter.current()
+            .notificationSettings()
         self.estadoDeLaAutorizacion = settings.authorizationStatus
     }
+
+    func solicitarPermiso() async {
+        do {
+            // lanza la modal el aviso de sistema una sola vez para que el usuario acepte o deniegue
+            // si la rechazamos nos llevara a los ajustes y el usuario lo tiene que hacer de froma manual
+            try await UNUserNotificationCenter.current().requestAuthorization(
+                options: [.alert, .badge, .sound])
+            await comprobarEstadoDePermisos()
+        } catch {
+            print(
+                "Error al solicitar los permisos: \(error.localizedDescription)"
+            )
+        }
+    }
+
+    // la funcion poara aplicar la notificación y programarla en el teléfono
+    func programaNotificacion(titulo: String, date: Date) {
+        // comprobamos que esta con permisos
+        guard estadoDeLaAutorizacion == .authorized else {
+            return
+        }
+        
+        //
+        let content = UNMutableNotificationContent()
+        content.title = "Recordatorio 📅"
+        content.body = titulo
+        content.sound = .defaultCritical
+        
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.hour, .minute], from: date)
+        
+        // el lanzador de la alerta . Si ponemos repeat a true se repetira cada día
+        let trigger = UNCalendarNotificationTrigger(dateMatching: components, repeats: false)
+        
+        let notificacion = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(notificacion) { error in
+            if let error = error {
+                print("Error al programar la notificación: \(error.localizedDescription)")
+            } else {
+                print("Notificación programada correctamente")
+            }
+        }
+    }
     
+    // TODO: funcion que puede sar llamada para eliminar la notificación programada
+    func cancelarNotificaciones() {
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+    }
+    
+    // TODO: Consultar las notificaciones pendientes
+    func notificacionesPendientes() async {
+        let notificacionesPendientes = await UNUserNotificationCenter.current().pendingNotificationRequests()
+    }
+    
+    // MARK: Métodos delegados ⤵️.- implementamos
+    // Caso de notificación .- cuando la app esta abierta
+    // se ejecuta en el hilo principal
+    @MainActor
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notication: UNNotification
+    ) async -> UNNotificationPresentationOptions {
+        print("Notificación recibida en primer plano")
+        return [.banner, .sound]
+    }
+
+    // Caso de notificación .- cuando esta cerrada o en background
+    @MainActor
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        didReceive response: UNNotificationResponse
+    ) async {
+        // El titulo del recordatorio lo hemos puesto en el body
+        // en la funcion programarNotificacion() y el usuario clica en la notificación nativa
+        let titulo = response.notification.request.content.body
+        
+        // para debug .-
+        print(
+            "El usuario ha pulsado la Notificación: \(titulo)"
+        )
+        
+        // cambiamos el ID del recordatorio .-
+        self.idRecordatorio = titulo
+    }
+
 }
--- Curso-ios-notificaciones/VistaPrincipal.swift
+++ Curso-ios-notificaciones/VistaPrincipal.swift
@@ -18,13 +18,14 @@ struct VistaPrincipal: View {
     // y se pide al usuario que los active
     // control
     @State private var mostrarAlertaAjustes = false
+    @State private var mostrarMensajeExito: Bool = false
     
     
     var body: some View {
         NavigationStack {
             Form {
                 Section("Datos") {
-                   TextField("Recordatorio", text: $recodatorio)
+                    TextField("Recordatorio", text: $recodatorio)
                     DatePicker("Hora", selection: $horaProgramada, displayedComponents: .hourAndMinute)
                 }
                 Section("Permisos") {
@@ -45,20 +46,92 @@ struct VistaPrincipal: View {
                 }
             }
             
+            
+            .navigationTitle(Text("Notificaciones"))
+            
+            // Cuando el usuario clica en la notificacion, se actualiza idRecordatorio y navega a VistaDetalle
+            .navigationDestination(item: Bindable(manager).idRecordatorio) { tituloRecordatorio in
+                VistaDetalle(texto: tituloRecordatorio)
+            }
+            
+            // ios tioene una comunicación interna NotificacionCenter entre la app y el sistema
+            // en el caso que el usuario viene de los ajustes al conceder o no los permisos
+            
+            // unidireccional .- sistema IOS a la app
+            // envio brodcats, y la app se registra para observar cambios en el sistema
+            .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
+                Task {
+                    await manager.comprobarEstadoDePermisos()
+                }
+            }
+            
+            
+            // closure de funciones
+            .alert("Permisos necesarios", isPresented: $mostrarAlertaAjustes) {
+                Button("Cancelar", role: .cancel) {}
+                Button("Ir a Ajustes") {
+                    // Este código nos abre la ventana de ajustes
+                    if let url = URL(string: UIApplication.openSettingsURLString) {
+                        UIApplication.shared.open(url)
+                    }
+                }
+            } message: {
+                Text("Haz desactivado las notificaciones, por favor activalas para poder usarla")
+            }
+            
+            // Popup .- Temporal de que la notificación se ha programado
+            .overlay(alignment: .bottom) {
+                if mostrarMensajeExito {
+                    Text("✅ Notificación programada")
+                        .padding()
+                        .background(.thinMaterial)
+                        .cornerRadius(10)
+                        .transition(.move(edge: .bottom))
+                        .padding(.bottom, 50)
+                    
+                }
+                
+            }
         }
-        .navigationTitle(Text("Notificaciones"))
         
-        VStack {
-            Image(systemName: "clock")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("La noticiación")
-        }
-        .padding()
     }
     
-    // TODO: aquio seguimos
+    // Según el estado hacemos una u otra cosa
     func programarAccionDeAviso() {
+        switch manager.estadoDeLaAutorizacion {
+        case .authorized:
+            
+            // TODO: pendiente
+            manager.programaNotificacion(titulo: recodatorio, date: horaProgramada)
+            
+            withAnimation{
+                mostrarMensajeExito = true
+            }
+            Task {
+                try? await Task.sleep(for: .seconds(1.5))
+                await MainActor.run {
+                    withAnimation {
+                        mostrarMensajeExito = false
+                    }
+                }
+            }
+            
+        case .notDetermined:
+            // al ser la primera vez, pedimos los permisos
+            Task {
+                await manager.solicitarPermiso()
+                if manager.estadoDeLaAutorizacion == .authorized {
+                    manager.programaNotificacion(titulo: recodatorio, date: horaProgramada)
+                }
+            }
+        case .denied:
+            // El usuario ha bloquedao los permisos y necesitamos una alerta para que el usuario desbloquee los permisos
+            mostrarAlertaAjustes = true
+            
+        default:
+            break
+            
+        }
         
     }
 }
@@ -84,6 +157,11 @@ struct VistaDetalle: View {
                 .background(Color.orange.opacity(0.2))
                 .cornerRadius(10)
             
+            // TODO: Necesitamos Programática para volver a la pantalla principal
+//            Button("Aceptar") {
+//                
+//            }
+            
         }
         .navigationTitle("Recordatorio")
     }
@@ -95,7 +173,7 @@ struct VistaEstadoPermisos: View {
     var body: some View {
         switch status {
         case .authorized:
-            Text("Permiso concedido").foregroundStyle(Color.green)
+            Text("Autorizado").foregroundStyle(Color.green)
         case .denied:
             Text("Denegado").foregroundStyle(Color.red)
         case .notDetermined:
